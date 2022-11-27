950902b12bb8f94cd7913adf5157191bba582556
Lydia <lydiarugby@gmail.com>
2022-11-27 14:57:56 +0100

    requete API produits

---
+++ front/js/script.js
@@ -0,0 +1,20 @@
+//Requêter l'API pour demander l'ensemble des produits
+
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json()) //pour récupérer la réponse au format json
+  .then((data) => {
+    console.log(data);
+    //utiliser une boucle pour créer les éléments pour chaque produit
+    data.forEach(produit => {
+      document.querySelector("#items").innerHTML += `<a href="./product.html?id=${produit._id}">
+                                                        <article>
+                                                          <img src="${produit.imageUrl}"  alt="${produit.altTxt}">
+                                                          <h3 class="productName" >${produit.name}</h3>
+                                                          <p class="productDescription">${produit.description}</p>
+                                                        </article>
+                                                    </a>`;
+    });
+  }) 
+  .catch(function(err) {
+    ("Une erreur est survenue")
+  });
